42a006133f9fa8a007cda71d0f43fb5fb3dd4623
Jordan Horwich <jordan@voidingwarranties.com>
2014-11-02 03:11:56 -0500

    Implement arm recognized callback

--- src/AtomicEventListener.h
+++ src/AtomicEventListener.h
@@ -19,9 +19,16 @@ class AtomicEventListener : public myo::DeviceListener {
     onOrientationDataCallback_(myo, timestamp, quat);
   }
 
+  void onArmRecognized(myo::Myo *myo, uint64_t timestamp, myo::Arm arm, myo::XDirection x_direction) {
+    onArmRecognizedCallback_(myo, timestamp, arm, x_direction);
+  }
+
   void setOnOrientationDataCallback(const std::function<
       void(myo::Myo *, uint64_t, const myo::Quaternion<float> &quat)>& callback) {
     onOrientationDataCallback_ = callback;
+  }
+  void setOnArmRecognizedCallback(const std::function<void(myo::Myo *, uint64_t, myo::Arm, myo::XDirection)>& callback) {
+    onArmRecognizedCallback_ = callback;
   }
 
  private:
@@ -29,4 +36,5 @@ class AtomicEventListener : public myo::DeviceListener {
   std::function<void(myo::Myo *)> onPeriodicCallback_;
   std::function<void(myo::Myo *, uint64_t, const myo::Quaternion<float> &)>
       onOrientationDataCallback_;
+  std::function<void(myo::Myo *, uint64_t, myo::Arm, myo::XDirection)> onArmRecognizedCallback_;
 };
