245c1cb8760de07c55614f65d88c089e2262c6a0
Jordan Horwich <jordan@voidingwarranties.com>
2015-03-23 20:56:31 -0700

    Use strings instead of stringstreams

--- tests/PrintEventsFeature.h
+++ tests/PrintEventsFeature.h
@@ -1,4 +1,5 @@
-#include <ostream>
+#include <string>
+#include <sstream>
 #include "../src/core/DeviceListenerWrapper.h"
 
 #define PRINT_NAME_AND_VAR(x) " "#x": " << x
@@ -12,7 +13,8 @@ std::ostream& operator<<(std::ostream& out,
   out << ", " << firmware_version.firmwareVersionHardwareRev << ")";
 }
 
-std::ostream& operator<<(std::ostream& out, const myo::Quaternion<float>& quat) {
+std::ostream& operator<<(std::ostream& out,
+                         const myo::Quaternion<float>& quat) {
   out << "(" << quat.x();
   out << ", " << quat.y();
   out << ", " << quat.z();
@@ -28,7 +30,7 @@ std::ostream& operator<<(std::ostream& out, const myo::Vector3<float>& quat) {
 
 class PrintEvents : public core::DeviceListenerWrapper {
  public:
-  PrintEvents(core::DeviceListenerWrapper& parent_feature, std::ostream& out);
+  PrintEvents(core::DeviceListenerWrapper& parent_feature, std::string& out);
 
   virtual void onPair(myo::Myo* myo, uint64_t timestamp,
                       myo::FirmwareVersion firmwareVersion) override;
@@ -58,140 +60,170 @@ class PrintEvents : public core::DeviceListenerWrapper {
                          const int8_t* emg) override;
 
  private:
-  std::ostream& out_;
+  std::string& out_;
 };
 
 PrintEvents::PrintEvents(core::DeviceListenerWrapper& parent_feature,
-                         std::ostream& out)
+                         std::string& out)
     : out_(out) {
   parent_feature.addChildFeature(this);
 }
 
 void PrintEvents::onPair(myo::Myo* myo, uint64_t timestamp,
                          myo::FirmwareVersion firmwareVersion) {
-  out_ << "onPair -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(firmwareVersion);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onPair -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(firmwareVersion);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onUnpair(myo::Myo* myo, uint64_t timestamp) {
-  out_ << "onUnpair -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onUnpair -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onConnect(myo::Myo* myo, uint64_t timestamp,
                             myo::FirmwareVersion firmwareVersion) {
-  out_ << "onConnect -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(firmwareVersion);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onConnect -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(firmwareVersion);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onDisconnect(myo::Myo* myo, uint64_t timestamp) {
-  out_ << "onDisconnect -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onDisconnect -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm,
                             myo::XDirection xDirection) {
-  out_ << "onArmSync -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(arm);
-  out_ << PRINT_NAME_AND_VAR(xDirection);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onArmSync -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(arm);
+  ss << PRINT_NAME_AND_VAR(xDirection);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onArmUnsync(myo::Myo* myo, uint64_t timestamp) {
-  out_ << "onArmUnsync -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onArmUnsync -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onUnlock(myo::Myo* myo, uint64_t timestamp) {
-  out_ << "onUnlock -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onUnlock -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onLock(myo::Myo* myo, uint64_t timestamp) {
-  out_ << "onLock -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onLock -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onPose(myo::Myo* myo, uint64_t timestamp,
                          const std::shared_ptr<core::Pose>& pose) {
-  out_ << "onPose -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(*pose);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onPose -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(*pose);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onGesture(myo::Myo* myo, uint64_t timestamp,
                             const std::shared_ptr<core::Gesture>& gesture) {
-  out_ << "onGesture -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(*gesture);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onGesture -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(*gesture);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onOrientationData(myo::Myo* myo, uint64_t timestamp,
                                     const myo::Quaternion<float>& rotation) {
-  out_ << "onOrientationData -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(rotation);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onOrientationData -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(rotation);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onAccelerometerData(myo::Myo* myo, uint64_t timestamp,
                                       const myo::Vector3<float>& accel) {
-  out_ << "onAccelerometerData -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(accel);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onAccelerometerData -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(accel);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onGyroscopeData(myo::Myo* myo, uint64_t timestamp,
                                   const myo::Vector3<float>& gyro) {
-  out_ << "onGyroscopeData -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(gyro);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onGyroscopeData -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(gyro);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onRssi(myo::Myo* myo, uint64_t timestamp, int8_t rssi) {
-  out_ << "onRssi -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << PRINT_NAME_AND_VAR(rssi);
-  out_ << "\n";
+  std::stringstream ss;
+  ss << "onRssi -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << PRINT_NAME_AND_VAR(rssi);
+  ss << "\n";
+  out_ += ss.str();
 }
 
 void PrintEvents::onEmgData(myo::Myo* myo, uint64_t timestamp,
                             const int8_t* emg) {
-  out_ << "onAccelerometerData -";
-  out_ << PRINT_NAME_AND_VAR(myo);
-  out_ << PRINT_NAME_AND_VAR(timestamp);
-  out_ << "emg: (";
-  out_ << emg[0];
+  std::stringstream ss;
+  ss << "onAccelerometerData -";
+  ss << PRINT_NAME_AND_VAR(myo);
+  ss << PRINT_NAME_AND_VAR(timestamp);
+  ss << "emg: (";
+  ss << emg[0];
   for (std::size_t i = 1; i < 8; ++i) {
-    out_ << ", " << emg[i];
+    ss << ", " << emg[i];
   }
-  out_ << ")\n";
+  ss << ")\n";
+  out_ += ss.str();
 }
